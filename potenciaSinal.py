8eed180ed26a58e974179198c3ba5fd90ece0886
lukkascost <lucas.costa@lit.ifce.edu.br>
2017-05-18 13:23:02 -0300

    método de estimação de distância funcionando

--- potenciaSinal.py
+++ potenciaSinal.py
@@ -1,23 +1,18 @@
 # -*- coding: cp1252 -*-
 from math import *
 potencias = [-x for x in range(1,102) ]
-pl0 = [-7-x for x in potencias]
-pt = 0
-pd0 = 43.2
-d0 = 0.1
-n = 1.3
-x0 = 5
-
-
-def distanciaPotenciaSinal(pl0, qtd, pt, pd0, d0, n, x0):
-    const1 = (-10)*log(pd0)
-    const2 = 10*n*log(d0)
-    const3 = const1 + const2 - x0 
+pl0 = [ -x for x in potencias]
+pd0 = -30
+n = 2.5
+def distanciaPotenciaSinal(potencias, qtd, pd0, n):
     for i in range (0, qtd):
-        const4 = const3 + pl0[i]
-        d1 = pow(e, const4/10*n)
-        print "DISTÂNCIA {:010.010f} \t POTÊNCIA {:03d}".format(d1,pl0[i])
+        expoente = ((pd0-potencias[i])/(10*n))
+        distance = pow(10, expoente)
+        mts = distance/100
+        print "DISTÂNCIA {:010.10f} \t POTÊNCIA {:03d}".format(mts,potencias[i])
 
-distanciaPotenciaSinal(pl0, len(pl0), pt, pd0, d0, n, x0)
+
+
+distanciaPotenciaSinal(potencias, len(potencias),pd0, n)
 
     
