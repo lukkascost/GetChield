0069b28538014491457dd2bb60a814411b80ab7f
Lucas Costa <lucas.costa@lit.ifce.edu.br>
2017-05-25 22:15:48 +0000

    medidas de 1 metro e calculo do N ideal

--- distance_1to1.py
+++ distance_1to1.py
@@ -2,11 +2,12 @@
 from math import *
 from serial import Serial
 import time
+import numpy as np
 measurements = [] 
 potencias = [-x for x in range(1,110) ]
 pl0 = [ -x for x in potencias]
 pd0 = 6
-n = 3.5
+n = 3.6
 def distanciaPotenciaSinal(potencias, qtd, pd0, n):
     res = []
     for i in range (0, qtd):
@@ -15,44 +16,17 @@ def distanciaPotenciaSinal(potencias, qtd, pd0, n):
         mts = distance/100
         res.append(mts)
     return res
-distances = distanciaPotenciaSinal(potencias, len(potencias),pd0, n)
-
-serialPort = Serial("/dev/ttyAMA0", 9600, timeout=2.0)	
-if (serialPort.isOpen() == False):
-    serialPort.open()
-serialPort.flushInput()
-serialPort.flushOutput()
-
-signal  = 0
-while(signal<1000):
-    print measurements
-    lista = []
-    time.sleep(2)
-    outStr = "AT+RSSI?"
-    inStr = ''
-    while (inStr is ""):
-        serialPort.write(outStr)
-        time.sleep(0.05)
-        inStr = serialPort.read(serialPort.inWaiting())
-
-        if(inStr is not ""):
-            signal += 1
-            for h,i in enumerate(inStr):
-                if i.isdigit() and h not in lista:
-                    lista.append(h)
-                    lista.append(h+1)
-            break
-        else:
-            print ""
-    valores = []
-    for i in range(0,len(lista),2):
-        valores.append(int(inStr[lista[i]])*10+int(inStr[lista[i+1]]))
-    [measurements.append(x) for x in valores]
-
-serialPort.close()
-
-arquivo = open("1Metro.txt", mode='w')
-for i in measurements:
-    arquivo.write(i)
-    arquivo.write("\n")
-arquivo.close()
+while(n<3.8):
+	distances = distanciaPotenciaSinal(potencias, len(potencias),pd0, n)
+	arquivo = open("1Metro.txt", mode='r')
+	real = []
+	distancia= []
+	for line in arquivo:
+    		for j in line.split(","):
+			real.append(int(j))
+			distancia.append(distances[int(j)])
+	arquivo.close()
+	real = np.array(real)
+	distancia = np.array(distancia)
+	print np.average(distancia), np.std(distancia), n
+	n = n+0.001
