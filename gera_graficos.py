693c8b2adee22b8145c2e967f82d224bf2f15993
Lucas  Silva <lucassilva@MacBook-Air-de-Lucas.local>
2017-06-01 15:17:23 -0300

    graficos de 1 em 1 metro

--- gera_graficos.py
+++ gera_graficos.py
@@ -1,23 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
-valors = np.zeros((10,1000))
-for i in [0,1,2,3,4,5,6,7,8,9]:
+valors = np.zeros((4,999))
+for h,i in enumerate([9,19,29,42]):
     arquivo = open("Medicoes/{:01.1f}Metro.txt".format(float(i+1)/10))
     print "Medicoes/{:01.1f}Metro.txt".format(float(i+1)/10)
     for j,k in enumerate(arquivo):
         if j == 999: break
-        valors[i,j] = int(k)
+        valors[h,j] = int(k)
     arquivo.close()
     print "ok"
-distancias = [float(x)/10 for x in [1,2,3,4,5,6,7,8,9,10]]
+distancias = [float(x)/10 for x in [10,20,30,43]]
+
+
 medias = [np.average(x) for x in valors]
 stDevian = [np.std(x) for x in valors]
+maximo = [np.max(x) for x in valors]
+minimo = [np.min(x) for x in valors]
 
-print distancias
-print medias
-print stDevian
-plt.plot(distancias,medias,label="Media")
-plt.plot(distancias,np.add(medias,stDevian), label="Med + std")
-plt.plot(distancias,np.subtract(medias,stDevian), label="Med - std")
+print valors
+plt.plot(distancias,maximo, label="max - std")
+plt.plot(distancias,minimo, label="max - std")
 plt.grid(True)
 plt.show()
