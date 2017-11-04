10fcd261b03396f334d5a500af42e330b48e30ab
lukkascost <lucas.costa@lit.ifce.edu.br>
2017-11-04 17:07:57 -0300

    file to generate bit array to send to the sigfox tower

---
+++ Generate_bitArray.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+date = datetime(17,01,30, 11,50,01)
+d1  = 10
+d2  = 2
+d3  = 11
+angulo = 290
+
+print date
+
+year = date.year
+
+
+day =       "{0:05b}".format(date.day)
+month =     "{0:04b}".format(date.month)
+year =      "{0:07b}".format(date.year)
+hour =      "{0:05b}".format(date.hour)
+minute =    "{0:06b}".format(date.minute)
+distance1 = "{0:04b}".format(d1)
+distance2 = "{0:04b}".format(d2)
+distance3 = "{0:04b}".format(d3)
+angle =     "{0:09b}".format(angulo)
+
+
+stringBit =  day+month+year+hour+minute+distance1+distance2+distance3+angle
+print stringBit
+print day,month,year,hour,minute,distance1,distance2,distance3,angle
+print hex(int(stringBit,2))[-12:].upper()
